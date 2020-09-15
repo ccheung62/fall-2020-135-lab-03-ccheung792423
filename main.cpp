3a651bfd46a033590906d60835579025435a75f5
Cheuk Lam Cheung <ccheung792423@gmail.com>
2020-09-15 19:14:59 -0400

    task b

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@
 
 int main()
 {
+  //Task A
   std::cout<< "Date: 01/01/2018\n" << "East basin storage: " << get_east_storage("01/01/2018") << " billion gallons\n\n"; 
   std::cout<< "Date: 12/31/2018\n" << "East basin storage: " << get_east_storage("12/31/2018") << " billion gallons\n\n";
   std::cout<< "Date: 05/15/2018\n" << "East basin storage: " << get_east_storage("05/15/2018") << " billion gallons\n\n";
   std::cout<< "Date: 02/28/2018\n" << "East basin storage: " << get_east_storage("02/28/2018") << " billion gallons\n\n";
   std::cout<< "Date: 09/16/2018\n" << "East basin storage: " << get_east_storage("09/16/2018") << " billion gallons\n\n";
 
+  //Task B
+  std::cout << "minimum storage in East basin: " << get_min_east() << " billion gallons\n\n";
+  std::cout << "MAXimum storage in East basin: " << get_max_east() << " billion gallons\n\n";
   return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -21,6 +21,47 @@ double get_east_storage(std::string date){
   }
   fin.close();
   // and return the correct value
-  return eastSt;
-  
+  return eastSt; 
+}
+
+double get_min_east() {
+  std::ifstream fin("Current_Reservoir_Levels.tsv");
+  if (fin.fail()) {
+    std::cerr << "File cannot be opened for reading." << std::endl;
+    exit(1); // exit if failed to open the file
+  }
+  std::string junk;        
+  getline(fin, junk); // read one line from the file to get rid of it
+  std::string date;
+  double eastSt;
+  double min = get_east_storage("01/01/2018");
+  while (fin >> date >> eastSt) {
+    fin.ignore(INT_MAX, '\n');
+    if (eastSt < min){
+      min = eastSt;
+    }
+  }
+  fin.close();
+  return min;
+}
+
+double get_max_east() {
+  std::ifstream fin("Current_Reservoir_Levels.tsv");
+  if (fin.fail()) {
+    std::cerr << "File cannot be opened for reading." << std::endl;
+    exit(1); // exit if failed to open the file
+  }
+  std::string junk;        
+  getline(fin, junk); // read one line from the file to get rid of it
+  std::string date;
+  double eastSt;
+  double max = get_east_storage("01/01/2018");
+  while (fin >> date >> eastSt) {
+    fin.ignore(INT_MAX, '\n');
+    if (eastSt > max){
+      max = eastSt;
+    }
+  }
+  fin.close();
+  return max; 
 }
