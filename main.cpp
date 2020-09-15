6b0fc233fae75886b8dfaa521d668eae7e959147
Cheuk Lam Cheung <ccheung792423@gmail.com>
2020-09-15 18:57:32 -0400

    task a

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 
 int main()
 {
-  double d = get_east_storage("somedate");
+  std::cout<< "Date: 01/01/2018\n" << "East basin storage: " << get_east_storage("01/01/2018") << " billion gallons\n\n"; 
+  std::cout<< "Date: 12/31/2018\n" << "East basin storage: " << get_east_storage("12/31/2018") << " billion gallons\n\n";
+  std::cout<< "Date: 05/15/2018\n" << "East basin storage: " << get_east_storage("05/15/2018") << " billion gallons\n\n";
+  std::cout<< "Date: 02/28/2018\n" << "East basin storage: " << get_east_storage("02/28/2018") << " billion gallons\n\n";
+  std::cout<< "Date: 09/16/2018\n" << "East basin storage: " << get_east_storage("09/16/2018") << " billion gallons\n\n";
+
   return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -6,9 +6,21 @@
 #include "reservoir.h"
 
 double get_east_storage(std::string date){
-  // add your code here
-
+  std::ifstream fin("Current_Reservoir_Levels.tsv");
+  if (fin.fail()) {
+    std::cerr << "File cannot be opened for reading." << std::endl;
+    exit(1); // exit if failed to open the file
+  }
+  std::string junk;        
+  getline(fin, junk); // read one line from the file to get rid of it
+  std::string currDate = "1/01/2018";
+  double eastSt;
+  while(date != currDate) { // check if it's looking at the date specified 
+    fin >> currDate >> eastSt; //take the first column as date and the second as east storage
+    fin.ignore(INT_MAX, '\n'); //skips to the end of line, ignorring the remaining columns
+  }
+  fin.close();
   // and return the correct value
-  return 0;
+  return eastSt;
   
 }
